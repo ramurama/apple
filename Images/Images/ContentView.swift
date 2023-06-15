75a433e096a89f77f6bd3250f86cf500893f1d6b
ramurama <rram.ramasamy@gmail.com>
2023-06-15 08:09:16 +0200

    initial commit

---
+++ Images/Images/ContentView.swift
@@ -0,0 +1,74 @@
+//
+//  ContentView.swift
+//  Images
+//
+//  Created by Ramu Ramasamy on 15.06.23.
+//
+
+import SwiftUI
+
+extension Image {
+    func imageModifier() -> some View {
+        self.resizable().scaledToFit()
+    }
+    
+    func iconModifier() -> some View {
+        self.imageModifier()
+            .frame(maxHeight: 130)
+            .foregroundColor(.purple)
+            .opacity(0.5)
+    }
+}
+
+struct ContentView: View {
+    
+    private let imageUrl: String = "https://credo.academy/credo-academy@3x.png"
+    
+    var body: some View {
+        VStack {
+
+            
+            Text("RAMU")
+                .font(.system(size: 100))
+                .fontWeight(.black)
+                .foregroundStyle(
+                    LinearGradient(
+                        colors: [.pink, .purple, .blue],
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing)
+                )
+            
+//            AsyncImage(url: URL(string: imageUrl)) {
+//                image in image.imageModifier()
+//            } placeholder: {
+//                Image(systemName: "photo.circle.fill").iconModifier()
+//            }.padding(40)
+            
+            AsyncImage(url: URL(string: imageUrl), transaction: Transaction(animation: .spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.25))) {
+                phase in switch phase {
+                case .success(let image):
+                    image.imageModifier()
+//                        .transition(.move(edge: .bottom))
+//                        .transition(.slide)
+                        .transition(.scale)
+                case .failure(_):
+                    Image(systemName: "ant.circle.fill").iconModifier()
+                case .empty:
+                    Image(systemName: "photo.circle.fill").iconModifier()
+                    
+                @unknown default:
+                    ProgressView()
+                }
+            }.padding(40)
+            
+            
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Images/Images/ImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagesApp.swift
+//  Images
+//
+//  Created by Ramu Ramasamy on 15.06.23.
+//
+
+import SwiftUI
+
+@main
+struct ImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
