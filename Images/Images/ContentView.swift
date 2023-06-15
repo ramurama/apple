152cb63effc18e986bc16d0e1e9eeef91d5fe3c2
Ramu Ramasamy <r.ramasamy@porta.de>
2023-06-15 09:44:26 +0200

    source format and swift enums and static functions in animation

--- Images/Images/ContentView.swift
+++ Images/Images/ContentView.swift
@@ -26,7 +26,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-
+            
             
             Text("RAMU")
                 .font(.system(size: 100))
@@ -38,18 +38,25 @@ struct ContentView: View {
                         endPoint: .bottomTrailing)
                 )
             
-//            AsyncImage(url: URL(string: imageUrl)) {
-//                image in image.imageModifier()
-//            } placeholder: {
-//                Image(systemName: "photo.circle.fill").iconModifier()
-//            }.padding(40)
+            //            AsyncImage(url: URL(string: imageUrl)) {
+            //                image in image.imageModifier()
+            //            } placeholder: {
+            //                Image(systemName: "photo.circle.fill").iconModifier()
+            //            }.padding(40)
             
-            AsyncImage(url: URL(string: imageUrl), transaction: Transaction(animation: .spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.25))) {
+            // the type is unknown here, hence the compiler does not know the origin of spring function
+            // let animation: = .spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.25)
+            
+            // here the type is known so no error
+            let animation: Animation = .spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.25)
+            let transaction = Transaction(animation: animation)
+            
+            AsyncImage(url: URL(string: imageUrl), transaction: transaction ) {
                 phase in switch phase {
                 case .success(let image):
                     image.imageModifier()
-//                        .transition(.move(edge: .bottom))
-//                        .transition(.slide)
+                    //                        .transition(.move(edge: .bottom))
+                    //                        .transition(.slide)
                         .transition(.scale)
                 case .failure(_):
                     Image(systemName: "ant.circle.fill").iconModifier()
