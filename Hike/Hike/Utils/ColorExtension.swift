a584133d79f65acfd027d7b9aad3178be2bba070
Ramu Ramasamy <r.ramasamy@porta.de>
2023-06-21 11:41:15 +0200

    hike app card and color extension
    Signed-off-by: Ramu Ramasamy <r.ramasamy@porta.de>

---
+++ Hike/Hike/Utils/ColorExtension.swift
@@ -0,0 +1,19 @@
+//
+//  ColorExtension.swift
+//  Hike
+//
+//  Created by Ramu Ramasamy on 21.06.23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let customGreenLight = Color("ColorGreenLight")
+    static let customGreenMedium = Color("ColorGreenMedium")
+    static let customGreenDark = Color("ColorGreenDark")
+    static let customGrayLight = Color("ColorGrayLight")
+    static let customGrayMedium = Color("ColorGrayMedium")
+    static let customIndigoMedium = Color("ColorIndigoMedium")
+    static let customSalmonLight = Color("ColorSalmonLight")
+}
