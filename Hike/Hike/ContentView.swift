7451afbdf1300cf2cbdce19677fb7603f69e921e
Ramu Ramasamy <r.ramasamy@porta.de>
2023-06-21 11:13:00 +0200

    hike app initial setup
    Signed-off-by: Ramu Ramasamy <r.ramasamy@porta.de>

---
+++ Hike/Hike/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by Ramu Ramasamy on 21.06.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("image-1")
+            .resizable()
+            .scaledToFit()
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hike/Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Ramu Ramasamy on 21.06.23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
